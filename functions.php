cfe1ca383c7451df55cc402518f16a824115f77b
Enguerrand-dev <57759112+Enguerrand-dev@users.noreply.github.com>
2022-01-25 13:47:40 +0100

    remove @
    modification require dans le thème

--- functions.php
+++ functions.php
@@ -1,9 +1,9 @@
 <?php
 // THEME SUPPORT
-@require 'utils/themes/support.php';
+require 'utils/themes/support.php';
 // ENQUEUE
-@require 'utils/enqueue.php';
+require 'utils/enqueue.php';
 // CUSTOM POST
-@require 'utils/posts/main.php';
+require 'utils/posts/main.php';
 // TAXONOMIES
-@require 'utils/taxonomies/main.php';
+require 'utils/taxonomies/main.php';
--- utils/posts/main.php
+++ utils/posts/main.php
@@ -1,3 +1,4 @@
 <?php
 
-// @require 'custom-post.php';
+require 'custom-post.php';
+require 'custom-post.php';
